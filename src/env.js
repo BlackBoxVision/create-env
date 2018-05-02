13fad308fcb66013f21e785530a6410284fcb236
jonatansalas <jonatansalas@live.com.ar>
2018-05-02 11:26:41 -0300

    Bump to v1.1.1

--- src/env.js
+++ src/env.js
@@ -2,6 +2,9 @@ const env = new Map();
 
 env.set("development", "DEV_");
 env.set("testing", "TEST_");
+
+//This is because you can use the environment name, or the branch. 
 env.set("production", "PROD_");
+env.set("master", "PROD_")
 
 module.exports = env;
